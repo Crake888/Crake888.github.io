a6a1349505b44ec33e3a0432ca6b39316244c0b2
Crake888 <116505784+Crake888@users.noreply.github.com>
2024-03-15 01:38:27 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,71 @@
+
+//---Navbar----------------------------------------------------------------------------------------
+
+window.addEventListener('scroll', function() {
+  var navbar = document.querySelector('.navbar');
+  var headerHeight = document.querySelector('.header').offsetHeight;
+
+  if (window.pageYOffset > headerHeight) {
+      navbar.style.position = 'fixed';
+      navbar.style.top = '0';
+      navbar.style.width = '100%';
+      navbar.style.backgroundColor = '#162e44'; /* Налаштуйте колір фону, якщо потрібно */
+      /* Додайте інші необхідні стилі */
+  } else {
+      navbar.style.position = 'static';
+      navbar.style.backgroundColor = '#162e44'; /* Поверніть фон в прозорий, якщо потрібно */
+      /* Поверніть інші стилі до значень за замовчуванням */
+  }
+});
+
+//---Hamburger-------------------------------------------------------------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu'),
+  menuItem = document.querySelectorAll('.menu_item'),
+  hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('menu_active');
+      })
+  })
+})
+
+//---Slider----------------------------------------------------------------------------------------
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+//-------------------------------------------------------------------------------------------
+
